22c3b701deee7feb2455d0ea59806453bc2ecea2
ricardj <43671359+ricardj@users.noreply.github.com>
2019-04-30 12:55:26 +0200

    Earth rotating.

--- js/scene.js
+++ js/scene.js
@@ -1,11 +1,16 @@
 var renderer;
 var scene;
 var camera;
+var cameraControl;
 
 
 function render(){
   console.log("Request animation frame for render");
   renderer.render(scene, camera);
+
+  cameraControl.update();
+  scene.getObjectByName('earth').rotation.y += 0.005;
+
 
   requestAnimationFrame(render);
 }
@@ -22,18 +27,22 @@ function createCamera (){
     45,
     window.innerWidth/ window.innerHeight,
     0.1, 1000);
-  camera.position.x = 15;
-  camera.position.y =  16;
-  camera.position.z = 13;
+  camera.position.x = 90;
+  camera.position.y =  32;
+  camera.position.z = 32;
   camera.lookAt(scene.position);
+
+  cameraControl = new THREE.OrbitControls(camera);
 }
 
 function createBox(){
   var boxGeometry = new THREE.BoxGeometry(6,4,6);
+/**
   var boxMaterial = new THREE.MeshLambertMaterial({
     color: "red"
   });
-
+*/
+  var boxMaterial = createEarthMaterial();
   var box = new THREE.Mesh(boxGeometry, boxMaterial);
   box.castShadow = true;
 
@@ -63,15 +72,39 @@ function createLight(){
   scene.add(spotLight);
 }
 
+function createEarthMaterial() {
+  var earthTexture = new THREE.Texture();
+  var loader = new THREE.ImageLoader();
+  loader.load('assets/earthmap2k.jpg', function(image){
+    earthTexture.image = image;
+    earthTexture.needsUpdate = true;
+
+  });
+
+  var earthMaterial = new THREE.MeshBasicMaterial();
+  earthMaterial.map = earthTexture;
+
+  return earthMaterial;
+}
+
+function createEarth(){
+  var sphereGeometry = new THREE.SphereGeometry(15,30,30);
+  var sphereMaterial = createEarthMaterial();
+  var earthMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  earthMesh.name = 'earth';
+  scene.add(earthMesh);
+}
 
 function init() {
 
   scene = new THREE.Scene();
   createRenderer();
   createCamera();
-  createBox();
+  //createBox();
+  //createPlane();
+  createEarth();
   createLight();
-  createPlane();
+  document.body.appendChild(renderer.domElement);
   render();
 }
 
